72c1c78bbc583a4c786aeb9705dfb2c58c4d93b6
NazarFedus <simpleprograming7@gmail.com>
2023-04-07 12:29:20 +0100

    add pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import { useState } from 'react'
-import './App.css'
+
+import './styles/main.css'
+import './styles/reset.css'
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Post from './pages/Post';
 
 function App() {
 
   return (
     <div className="App">
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/post' element={<Post />}></Route>
+        </Routes>
+      </Router>
 
     </div>
   )
